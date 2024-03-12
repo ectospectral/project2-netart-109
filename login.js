1ec256ebf9e95f94ba71c3da7552de434dad2891
ectospectral <132393822+ectospectral@users.noreply.github.com>
2024-03-12 13:34:41 -0700

    final commit
    I wanted this to be better, but here we are

---
+++ login.js
@@ -0,0 +1,15 @@
+
+//if you can see this, you're cheating, but that's fine.
+
+let correctPassword = 'kraken';
+
+function enterPassword(){
+    if(document.querySelector('#ishmael').value != correctPassword){
+        alert('Incorrect Password');
+        return false;
+    }
+    else if(document.querySelector('#ishmael').value === correctPassword){
+        alert('Welcome back.')
+        return true;
+    }
+}
--- main.js
+++ main.js
@@ -1,15 +1,33 @@
+let img0= document.querySelector('#image-0');
+let img1= document.querySelector('#image-1');
+let img2= document.querySelector('#image-2');
+let img3= document.querySelector('#image-3');
+let img4= document.querySelector('#image-4');
 
-//if you can see this, you're cheating, but that's fine.
+let button = document.querySelector('#btn');
 
-let correctPassword = 'kraken';
+img0.addEventListener('click', function(){
+    img2.style.visibility = 'visible';
+});
+img1.addEventListener('click', function(){
+    img3.style.visibility = 'visible';
+    img4.style.visibility = 'visible';
+});
+img2.addEventListener('click', function(){
+    img1.style.visibility = 'visible';
+    img0.style.visibility = 'hidden';
+});
+img3.addEventListener('click', function(){
+    img0.style.visibility = 'visible';
+    img2.style.visibility = 'hidden'; 
+    button.style.visibility = 'visible';
+});
+img4.addEventListener('click', function(){
+    img0.style.visibility = 'visible';
+    img1.style.visibility = 'hidden';
+    img2.style.visibility = 'hidden';
+    img3.style.visibility = 'hidden';
+    img4.style.visibility = 'hidden';
+});
 
-function enterPassword(){
-    if(document.querySelector('#ishmael').value != correctPassword){
-        alert('Incorrect Password');
-        return false;
-    }
-    else if(document.querySelector('#ishmael').value === correctPassword){
-        alert('Welcome back.')
-        return true;
-    }
-}
+
